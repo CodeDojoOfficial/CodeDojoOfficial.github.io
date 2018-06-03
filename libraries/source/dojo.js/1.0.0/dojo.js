c579c48ddfcb5c2e306972d433cb2193a53638b7
Adrian Gjerstad <adriangjerstad@lances-mac-mini.lan>
2018-06-03 15:25:20 -0400

    Dojo.js additions

--- libraries/source/dojo.js/1.0.0/dojo.js
+++ libraries/source/dojo.js/1.0.0/dojo.js
@@ -9,9 +9,15 @@ let $b = document.body;
 
 let canvas;
 let canvasContext;
+let width;
+let height;
 let fps = 30;
+let clock;
 let paintFill = "rgba(0, 0, 0, 0)";
-let paintStroke = "rgba(0, 0, 0, 1);
+let paintStroke = "rgba(0, 0, 0, 1)";
+
+let translationX;
+let translationY;
 
 /**
  * Creates the canvas and canvas context used to draw on the window.
@@ -42,6 +48,12 @@ function createWindow(windowWidth, windowHeight) {
   $b.appendChild(canvas);
   
   canvasContext = canvas.getContext("2d");
+  
+  width = Math.abs(windowWidth);
+  height = Math.abs(windowHeight);
+  
+  translationX = 0; // Start at left
+  translationY = 0; // Start at top
   
   return canvas;
 }
@@ -79,6 +91,48 @@ function fill(red, green, blue, alpha) {
   }
 }
 
+function stroke(red, green, blue, alpha) {
+  red = clamp(red, 0, 255);
+  green = clamp(green, 0, 255);
+  blue = clamp(blue, 0, 255);
+  alpha = clamp(alpha, 0, 255);
+  
+  if(red !== undefined && green === undefined && blue === undefined && alpha === undefined) {
+    // Grayscale full alpha.
+    
+    paintStroke = "rgba(" + red + ", " + red + ", " + red + ", 1)";
+  } else if(red !== undefined && green !== undefined && blue === undefined && alpha === undefined) {
+    // Grayscale depending alpha.
+    
+    paintStroke = "rgba(" + red + ", " + red + ", " + red + ", " + map(green, 0, 255, 0, 1) + ")";
+  } else if(red !== undefined && green !== undefined && blue !== undefined && alpha === undefined) {
+    // RGB full alpha.
+    
+    paintStroke = "rgba(" + red + ", " + green + ", " + blue + ", 1)";
+  } else {
+    // RGB depending alpha.
+    
+    paintStroke = "rgba(" + red + ", " + green + ", " + blue + ", " + map(alpha, 0, 255, 0, 1) + ")";
+  }
+}
+
+
+// DRAW COMMANDS
+function setPixel(x, y, red, green, blue, alpha) {
+  red = clamp(red, 0, 255);
+  green = clamp(green, 0, 255);
+  blue = clamp(blue, 0, 255);
+  alpha = clamp(alpha, 0, 255);
+  
+  canvasContext.fillStyle = "rgba(" + red + ", " + green + ", " + blue + ", " + map(alpha, 0, 255, 0, 1) + ")";
+  canvasContext.fillRect(x, y, 0, 0);
+}
+
+function point(x, y) {
+  canvasContext.fillStyle = paintStroke;
+  canvasContext.
+}
+
 function wait(millis, func) {
   if(millis > 0) {
     setTimeout(millis, func);
@@ -90,6 +144,8 @@ function wait(millis, func) {
 function frames(newFps) {
   if(newFps > 0) {
     fps = newFps;
+    clearInterval(clock);
+    clock = setInterval(1000/fps, paint);
   } else if(newFps >= 60) {
     console.warn("Your browser may slow down due to high loop rates.");
   } else {
@@ -129,7 +185,7 @@ window.onload = function() {
     
     setup();
     
-    setInterval(1000/fps, paint);
+    clock = setInterval(1000/fps, paint);
     
   } catch(e) {
     //console.info("Not every command is used.");
